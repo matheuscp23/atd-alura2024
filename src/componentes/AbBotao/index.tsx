561805473cf35c2a4fa02a2939002cf01ad1b9ce
matheuscp23 <160146795+matheuscp23@users.noreply.github.com>
2024-05-13 13:51:38 -0300

    adicionando variantes do AbBotao

--- src/componentes/AbBotao/index.tsx
+++ src/componentes/AbBotao/index.tsx
@@ -1,22 +1,39 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const BotaoEstilizado = styled.button`
-background: #EB9B00;
+export interface AbBotaoProps{
+  texto?: string,
+  tipo?: 'primario' | 'secundario',
+  onClick?: () => void
+}
+
+const BotaoEstilizado = styled.button<AbBotaoProps>`
+background: ${(props:AbBotaoProps)=> props.tipo === 'primario' ? '#EB9B00' : '#FFF'};
 padding: 16px 32px;
 border: 2px solid #EB9B00;
-color: #fff;
+color: ${(props:AbBotaoProps)=> props.tipo === 'primario' ? '#FFF' : '#EB9B00'};
 font-size: 20px;
 cursor: pointer;
-
-&:hover{
+${(props:AbBotaoProps)=> props.tipo === 'primario' 
+? css`
+  &:hover{
     background: #B87900;
     border: 2px solid#B87900;
 }
+` 
+: css`
+  &:hover{
+    background: #FFF;
+    border: 2px solid#B87900;
+    color: #B87900;
+}`};
+
 `
 
-export const AbBotao = () => {
+export const AbBotao = ({texto, onClick, tipo = 'primario'}: AbBotaoProps)=> {
   return (
-    <BotaoEstilizado>Clique aqui !</BotaoEstilizado>
+    <BotaoEstilizado onClick={onClick} tipo={tipo}>
+      {texto}
+      </BotaoEstilizado>
   )
 }
--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { AbBotao }from '../src'
+import { AbBotao, AbBotaoProps }from '../src'
 export default{
     title: 'Componentes/AbBotao',
     component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao/>
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args}/>
 
 export const Primario = Template.bind({})
+Primario.args = {
+    texto: 'Ab botão Primário',
+    tipo: 'primario'
+}as AbBotaoProps
+
+export const Secundario = Template.bind({})
+Secundario.args = {
+    texto: 'Ab botão Secundario',
+    tipo: 'secundario'
+}as AbBotaoProps
